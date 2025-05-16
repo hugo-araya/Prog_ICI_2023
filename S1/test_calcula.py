58767ff9e945bf6fd24a202d5c29a706c9f5a8ca
Hugo Araya C. <42989121+hugo-araya@users.noreply.github.com>
2025-05-16 10:08:50 -0400

    kii

--- S1/test_calcula.py
+++ S1/test_calcula.py
@@ -4,15 +4,16 @@ import calcula
 class TestCalcula(unittest.TestCase):
     def test_suma(self):
         self.assertEqual(calcula.suma(20,10), 30)
-    
+        self.assertEqual(calcula.suma(50,10), 30)
+
     def test_resta(self):
         self.assertEqual(calcula.resta(20,10), 10)
 
     def test_multi(self):
-        self.assertEqual(calcula.resta(20,10), 10)
+        self.assertEqual(calcula.multi(20,10), 200)
 
     def test_divide(self):
-        self.assertEqual(calcula.resta(20,10), 10)
+        self.assertEqual(calcula.divide(20,10), 2)
 
 if __name__ == '__main__':
     unittest.main()
